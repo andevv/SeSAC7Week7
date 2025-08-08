ececee02eaa411d7252da0a4ea6557eaf3409598
andev <andh1006@naver.com>
2025-08-08 09:59:57 +0900

    Initial Commit

---
+++ SeSAC7Week7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Week7
+//
+//  Created by andev on 8/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
